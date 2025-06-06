5eb92ee4b0614513876cb664f2b2c8ab75de9ac8
Thomson1511 <kovacstomi130@gmail.com>
2025-06-06 21:55:27 +0200

    mostmár működik pages-en

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/atco-training', // Ez legyen a repository neve, pl. /<repo-név>
+});
